cc9cc52f9910547361f241e614b7ce4b9e4dfa34
gcp <oilstand@shimesaba.jp>
2021-01-09 07:27:29 +0000

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="main-body">
+    <header id="header">
+        <h1 style="">
+            <img src="icon.png" style="width:48px;height:48px;">
+            <router-link to="/">
+                OILSTAND
+            </router-link>
+        </h1>
+    </header>
+    <nuxt />
+    <footer>
+        &copy; OILSTAND
+    </footer>
+  </div>
+</template>
+
+<style>
+* {
+    margin:0;padding:0;
+}
+body {
+    background-color:#F9F9F9;
+}
+.main-body {
+}
+header {
+    display:flex;
+}
+header h1 {
+    height:100%;
+    display:flex;
+    align-items: center;
+}
+header h1 a {
+    display:block;
+    height:100%;
+    position:relative;
+    vertical-align:middle;
+
+    color:#2d2d2d;
+    font-weight: 500;
+}
+@media screen and (max-width:959px) {
+    header {
+        height:50px;
+        padding: 8px 16px;
+    }
+    header h1 a {
+        line-height:50px;
+
+        padding-left:calc(32px + 8px);
+        font-size:24px;
+    }
+
+    #menu_sitemap {
+        display:none;
+    }
+    footer {
+        margin-top: 24px;
+        padding: 16px 8px;
+        position:relative;
+    }
+}
+@media screen and (min-width:960px) {
+    header {
+        height:90px;
+        padding: 16px 32px;
+    }
+    header h1 a {
+        line-height:90px;
+        padding-left:calc(16px);
+    }
+    footer {
+        margin-top: 80px;
+        padding: 8px 20px;
+
+        width:100%;
+        left:0;bottom:0;
+        text-align:center;
+    }
+}
+footer {
+    background-color:#707070;
+    color:white;
+}
+footer > div {
+    max-width:1200px;
+    margin:0 auto;
+}
+footer h2 {
+    margin: 16px 0;
+}
+</style>
+
+<script>
+export default {
+  head() {
+      return {
+            htmlAttrs: {
+                lang: 'ja',
+                prefix: 'og: http://ogp.me/ns#'
+            },
+            title: 'OILSTAND',
+            meta: [
+                { hid: 'description', name: 'description', content: `OILSTAND` },
+                { hid: 'keywords', name: 'keywords', content: 'OILSTAND' },
+                { hid: 'og:site_name', property: 'og:site_name', content: 'OILSTAND' },
+                { hid: 'og:type', property: 'og:type', content: 'website' },
+                { hid: 'og:url', property: 'og:url', content: 'https://oilstand.net' + this.$nuxt.$route.path },
+                { hid: 'og:title', property: 'og:title', content: `OILSTAND` },
+                { hid: 'og:description', property: 'og:description', content: 'OILSTAND' },
+                { hid: 'og:image', property: 'og:image', content: 'https://oilstand.net/logo.png' },
+                { name: 'twitter:card', content: 'summary' }
+            ],
+      }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  computed: {
+  },
+  mounted() {
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,437 @@
+<template>
+    <div style="max-width:960px;margin:0 auto;">
+        <ul class="products-list">
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://www.circlecheck.site/favicon.ico" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>サークルチェックサイト</h3>
+                        <a href="https://www.circlecheck.site">https://www.circlecheck.site</a>
+                    </div>
+                    <span class="since-until_badge" style="">2016-</span>
+                </div>
+                <div class="description" style="">
+                    同人誌即売会の参加者チェックが出来るサイト。<br>
+                    委託販売や即売会でのスペース位置・即売会のおしながきなどの情報取集が出来る。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">GCP</li>
+                    <li style="">GAE</li>
+                    <li style="">CloudSQL</li>
+                    <li style="">PHP</li>
+                    <li style="">TwitterAPI</li>
+                    <li style="">GitHub / Cloud Source Repositories</li>
+                    <li style="">GTM / GA</li>
+                </ul>
+                <table class="achievement_table" style="">
+                    <thead>
+                        <tr><th></th><th>UU</th><th>PV</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><th>2020</th><td>245,274</td><td>696,149</td></tr>
+                        <tr><th>2019</th><td>366,550</td><td>2,071,924</td></tr>
+                        <tr><th>2018</th><td>166,874</td><td>1,330,242</td></tr>
+                    </tbody>
+                </table>
+            </li>
+
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://bookcell.net/favicon.ico" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>Bookcell</h3>
+                        <a href="https://bookcell.net/">https://bookcell.net/</a>
+                    </div>
+                    <span class="since-until_badge" style="">2019-</span>
+                </div>
+                <div class="description" style="">
+                    タイル状にブックマークをまとめられるサイト。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">GCP</li>
+                    <li style="">GAE</li>
+                    <li style="">JavaScript</li>
+                    <li style="">Firebase Authentication</li>
+                    <li style="">GitHub</li>
+                    <li style="">GA</li>
+                </ul>
+            </li>
+
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://livetimelineshift.appspot.com/icon.png" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>あとから同時視聴 / 複窓コラボ</h3>
+                        <a href="https://livetimelineshift.appspot.com/">https://livetimelineshift.appspot.com/</a>
+                    </div>
+                    <span class="since-until_badge" style="">2020-</span>
+                </div>
+                <div class="description" style="">
+                    YouTube Liveアーカイブに後からコメントを付けられるサイト。<br>
+                    複数のYouTube Liveアーカイブを配信時時間軸で同期再生することができる。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">GCP</li>
+                    <li style="">GAE</li>
+                    <li style="">Google Cloud Datastore</li>
+                    <li style="">Laravel</li>
+                    <li style="">PHP</li>
+                    <li style="">node</li>
+                    <li style="">nuxt(SSR)</li>
+                    <li style="">vue</li>
+                    <li style="">vuetify</li>
+                    <li style="">JavaScript</li>
+                    <li style="">TwitterAPI</li>
+                    <li style="">YouTube Data API v3</li>
+                    <li style="">Firebase Authentication</li>
+                    <li style="">GitHub</li>
+                    <li style="">GTM / GA</li>
+                </ul>
+            </li>
+
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://akukin.jp/icon.png" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>AKUKIN建設 コーポレートサイト</h3>
+                        <a href="https://akukin.jp/">https://akukin.jp/</a>
+                    </div>
+                    <span class="since-until_badge" style="">2020.10-</span>
+                </div>
+                <div class="description" style="">
+                    VTuber「湊あくあ」のMinecraft内での企業「AKUKIN建設」のネタサイト。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">GCP</li>
+                    <li style="">GAE</li>
+                    <li style="">node</li>
+                    <li style="">nuxt(static)</li>
+                    <li style="">vue</li>
+                    <li style="">JavaScript</li>
+                    <li style="">GitHub</li>
+                    <li style="">GTM / GA</li>
+                </ul>
+                <table class="achievement_table" style="">
+                    <thead>
+                        <tr><th></th><th>UU</th><th>PV</th></tr>
+                    </thead>
+                    <tbody>
+                        <tr><th>2020</th><td>33,605</td><td>320,744</td></tr>
+                    </tbody>
+                </table>
+            </li>
+
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://holoechelon.com/icon.png" style="background-color:rgba(0,0,0,.3)">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>HoloEchelon</h3>
+                        <a href="https://holoechelon.com/">https://holoechelon.com/</a>
+                    </div>
+                    <span class="since-until_badge" style="">2020-</span>
+                </div>
+                <div class="description" style="">
+                    ホロライブ所属VTuberの配信スケジュール確認サイト
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">GCP</li>
+                    <li style="">GAE</li>
+                    <li style="">Google Cloud Datastore</li>
+                    <li style="">Laravel</li>
+                    <li style="">PHP</li>
+                    <li style="">node</li>
+                    <li style="">nuxt(SSR)</li>
+                    <li style="">vue</li>
+                    <li style="">JavaScript</li>
+                    <li style="">TwitterAPI</li>
+                    <li style="">YouTube Data API v3</li>
+                    <li style="">GitHub</li>
+                    <li style="">GTM / GA</li>
+                </ul>
+            </li>
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="/rench.png" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3 style="font-size:0.85em;">Nintendo Switch 湊あくあエディション</h3>
+                        <a href="">close</a>
+                    </div>
+                    <span class="since-until_badge" style="">2020.5</span>
+                </div>
+                <div class="description" style="">
+                    海鼠先生から許可をいただき、ファンアートをグレースケール化&印刷できるフォーマットへ加工。<br>
+                    FabLabの機器を利用してSwitchへ印刷した。<br>
+                    ジョイコン用シェルは海外から購入。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">Adobe Illustrator</li>
+                    <li style="">UVプリンター Mimaki UJF-3042HG(FabLab)</li>
+                    <li style="">(検証利用)レーザーカッター trotec Speedy300(FabLab)</li>
+                </ul>
+                <blockquote class="twitter-tweet">
+                    <p lang="ja" dir="ltr">Nintendo Switch 湊あくあエディションが完成！！<br>イラストは海鼠先生(<a href="https://twitter.com/NAMCOOo?ref_src=twsrc%5Etfw">@NAMCOOo</a>)からお借りしました<a href="https://twitter.com/hashtag/%E3%81%82%E3%81%8F%E3%81%82%E3%83%BC%E3%81%A8?src=hash&amp;ref_src=twsrc%5Etfw">#あくあーと</a> <a href="https://t.co/siZvYsyLuM">pic.twitter.com/siZvYsyLuM</a></p>&mdash; OILSTAND (@OIL_STAND) <a href="https://twitter.com/OIL_STAND/status/1257243958352031745?ref_src=twsrc%5Etfw">May 4, 2020</a>
+                </blockquote>
+            </li>
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="/telephone.png" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>名取さな電話</h3>
+                        <a>公開終了</a>
+                    </div>
+                    <span class="since-until_badge" style="">2018.7-2018.10</span>
+                </div>
+                <div class="description" style="">
+                    公開されている電話番号へ電話をかけるとVTuber「名取さな」のPRボイスを聞くことができる。<br>
+                    ファンコンテンツ。<br>
+                    番号維持・通話時間によるコスト問題があったためクローズ。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">さくらのVPS</li>
+                    <li style="">PHP</li>
+                    <li style="">Twilio</li>
+                </ul>
+                <blockquote class="twitter-tweet">
+                    <p lang="ja" dir="ltr">「名取さな電話」つくった<br>1ヶ月くらいの期間限定で公開できそうなので電番モザイク無し版あげとく<br><br>※実際にかけられます<br>※国内発信の通話料金かかります<br>※電話番号残したくない場合は非通知か公衆電話でかけてね<br>※予告無く終了するかも<a href="https://twitter.com/hashtag/%E5%90%8D%E5%8F%96%E3%81%95%E3%81%AA?src=hash&amp;ref_src=twsrc%5Etfw">#名取さな</a> <a href="https://twitter.com/hashtag/%E3%83%8C%E3%82%A9%E3%83%B3%E3%82%BF%E3%83%BC%E3%83%88?src=hash&amp;ref_src=twsrc%5Etfw">#ヌォンタート</a> ？ <a href="https://t.co/5C3fZbxnP2">pic.twitter.com/5C3fZbxnP2</a></p>&mdash; OILSTAND (@OIL_STAND) <a href="https://twitter.com/OIL_STAND/status/1014113806677446656?ref_src=twsrc%5Etfw">July 3, 2018</a>
+                </blockquote>
+            </li>
+
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://holoechelon.com/favicon.ico" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>ブログ2</h3>
+                        <a>close</a>
+                    </div>
+                    <span class="since-until_badge" style="">2014.6-2021.1</span>
+                </div>
+                <div class="description" style="">
+                    2015.4で更新終了。<br>
+                    記事の記述、画像のアップロード、時限開放などのブログに必要な機能が含まれたCMS。<br>
+                    CodeIgniterを使って実装。<br>
+                    当時プレイしていたオンラインゲームの攻略情報などをまとめていた。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">さくらのVPS</li>
+                    <li style="">CodeIgniter</li>
+                    <li style="">PHP</li>
+                    <li style="">MySQL</li>
+                    <li style="">GA</li>
+                </ul>
+            </li>
+
+            <li style="">
+                <div class="thumbnail" style="">
+                    <img src="https://holoechelon.com/favicon.ico" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>ブログ1</h3>
+                        <a>close</a>
+                    </div>
+                    <span class="since-until_badge" style="">2013.6-2014.6</span>
+                </div>
+                <div class="description" style="">
+                    記事の記述、画像のアップロード、時限開放などのブログに必要な機能が含まれたCMS。<br>
+                    当時プレイしていたオンラインゲームの攻略情報などをまとめていた。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">さくらのレンタルサーバー</li>
+                    <li style="">PHP</li>
+                    <li style="">MySQL</li>
+                    <li style="">GA</li>
+                </ul>
+            </li>
+
+            <li v-if="false" style="">
+                <div class="thumbnail" style="">
+                    <img src="https://holoechelon.com/favicon.ico" style="">
+                </div>
+                <div class="product-info" style="">
+                    <div style="">
+                        <h3>ブログ(アーカイブ)</h3>
+                        <a>close</a>
+                    </div>
+                    <span class="since-until_badge" style="">2021.1-</span>
+                </div>
+                <div class="description" style="">
+                    2015.4で更新終了。<br>
+                    記事の記述、画像のアップロード、時限開放などのブログに必要な機能が含まれたCMS。<br>
+                    CodeIgniterを使って実装。<br>
+                    当時プレイしていたオンラインゲームの攻略情報などをまとめていた。
+                </div>
+                <ul class="tech_list" style="">
+                    <li style="">さくらのVPS</li>
+                    <li style="">CodeIgniter</li>
+                    <li style="">PHP</li>
+                    <li style="">MySQL</li>
+                    <li style="">GA</li>
+                </ul>
+            </li>
+
+        </ul>
+    </div>
+</template>
+<style>
+.tech_list {
+    display:flex;
+    flex-wrap:wrap;
+    list-style: none;
+    width: 100%;
+}
+.tech_list > li {
+    border-radius:0.5em;
+    border:solid 1px gray;
+    margin-right: 16px;
+    margin-bottom:8px;
+    padding: 2px 8px;
+}
+.achievement_table {
+    width: calc(100% - 32px);
+    margin: 16px 16px;
+    border: solid 1px gray;
+    text-align:center;
+}
+.achievement_table th,
+.achievement_table td {
+    border:solid 1px gray;
+}
+@media screen and (max-width:959px) {
+    .products-list > li {
+        padding: 16px;
+    }
+    .thumbnail {
+        width:80px;
+        height:90px;
+    }
+    .thumbnail > img {
+        width: 60px;
+        height: 60px;
+        margin-top:15px;
+    }
+    .product-info {
+        width: calc(100% - 80px);
+        height:90px;
+    }
+    .since-until_badge {
+        right:0;
+        top:0;
+        font-size:0.6em;
+    }
+    .product-info h3 {
+        font-size:1.0em;
+    }
+    .product-info a {
+        font-size:0.9em;
+    }
+    .description {
+        padding: 8px 0;
+    }
+}
+@media screen and (min-width:960px) {
+    .products-list > li {
+        padding: 16px 30px;
+    }
+    .thumbnail {
+        width:120px;
+    }
+    .thumbnail > img {
+        width: 90px;
+        height: 90px;
+    }
+    .product-info {
+        width: calc(100% - 120px);
+    }
+    .since-until_badge {
+        right:3em;
+        top:1em;
+    }
+    .description {
+        padding: 16px 0;
+    }
+}
+.products-list > li {
+    display:flex;
+    flex-wrap:wrap;
+}
+.product-info {
+    position: relative;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.since-until_badge {
+    display:block;
+    position:absolute;
+    border-radius:0.5em;
+    border:solid 1px gray;
+    padding: 2px 8px;
+}
+
+</style>
+<script>
+export default {
+    data () {
+        return {
+        }
+    },
+    methods: {
+        twitterWidgetLoad() {
+            if(!window.twttr) {
+                window.twttr = (function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0],
+                        t = window.twttr || {};
+                    if (d.getElementById(id)) return t;
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = "https://platform.twitter.com/widgets.js";
+                    fjs.parentNode.insertBefore(js, fjs);
+
+                    t._e = [];
+                    t.ready = function(f) {
+                        t._e.push(f);
+                    };
+
+                    return t;
+                }(document, "script", "twitter-wjs"));
+            } else {
+                twttr.widgets.load();
+            }
+        }
+    },
+    computed: {
+    },
+    mounted() {
+        this.twitterWidgetLoad();
+    },
+    head() {
+        return {
+            title: '',
+            titleTemplate: 'OILSTAND',
+        }
+    }
+}
+</script>
